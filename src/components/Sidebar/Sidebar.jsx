8745365eadc104b19046b055204e2b8f153540bd
Surkho <asa8964@mail.ru>
2022-08-31 14:46:08 +0300

    добавил сайдбар

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import style from './sidebar.module.css'
+import burger from '../../images/menu.png'
+import Menu from './Menu.jsx'
+import { useState } from 'react';
 
 const Sidebar = () => {
+
+    const [menuActive, setMenuActive] = useState(false)
+  const  handleMenu = () =>{
+    setMenuActive(true)
+  }
     return (
-        <div>
+        <div className={style.burger_btn_container}>
+            <img src={burger} alt="burger-btn"  className={style.burger_btn}  onClick={() => handleMenu()}/>
             
+            <Menu  active={menuActive} setActive={setMenuActive} />
+        
         </div>
     );
 };
