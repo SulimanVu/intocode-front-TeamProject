ab0192093e328ece92fe6905ef5cd9b6f6ba6fa4
Waldgang <waldgang@MacBook-Air-salambek.local>
2022-08-31 13:40:29 +0300

    Добавил стиль для кнопки добавления студента

--- src/pages/MainPage/Mainpage.jsx
+++ src/pages/MainPage/Mainpage.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import logo from "../../images/intocode_3.png";
 
 const Mainpage = () => {
+  
   const [login, setLogin] = useState('')
   const [password, setPassword] = useState('')
   const [form, setForm] = useState(false)
