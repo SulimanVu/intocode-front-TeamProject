a5cc497a7c4e1d9bdea1a8f9a354055b680ccdc7
Waldgang <waldgang@MacBook-Air-salambek.local>
2022-08-31 14:44:43 +0300

    раскомментировал store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import application from '../features/applicationSlice'
-// import student from '../features/studentSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import application from '../features/applicationSlice'
+import student from '../features/studentSlice'
+import note from '../features/noteSlice'
 
-// export const store = configureStore({
-//     reducer: {application, student}
-// })
+export const store = configureStore({
+    reducer: {application, student, note}
+})
